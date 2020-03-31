ed89ef95a6af1d244b3b1668f633c72f005428e1
Alex <AlexKozlovDeveloper@gmail.com>
2020-03-31 23:41:22 +0300

    Add Project

---
+++ Froze.NetFeatures/Features/AsyncAwaitFeature.cs
@@ -0,0 +1,25 @@
+﻿using Froze.Core.Logging;
+using Froze.NetFeatures.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froze.NetFeatures.Features
+{
+    class AsyncAwaitFeature : IFeature
+    {
+        private readonly ILogger _logger;
+
+        public AsyncAwaitFeature(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
