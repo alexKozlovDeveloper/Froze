3bf54ca8eade2deeef270fdf4391726ca4c18ef3
Alex <AlexKozlovDeveloper@gmail.com>
2020-04-02 00:54:09 +0300

    Add Test project

---
+++ Froze.Core.Test/Logging/LoggerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Froze.Core.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Froze.Core.Test
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void Test_Create()
+        {
+            try
+            {
+                var logger = new Logger();
+
+                logger.Info("Test Info");
+                logger.Warn("Test Warn");
+                logger.Error("Test Error [string]");
+                logger.Error(new NotSupportedException("Test Error [exception]"));
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            
+        }
+    }
+}
---
+++ Froze.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Froze.Core.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Froze.Core.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a1da0a2-086b-460d-af84-9dd472fe63ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
