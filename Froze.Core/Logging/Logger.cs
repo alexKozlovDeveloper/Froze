20d35752aa906035573eb56a9fe6abd3d284bc06
Alex <AlexKozlovDeveloper@gmail.com>
2020-04-02 00:53:52 +0300

    Add logger

---
+++ Froze.Core/Logging/Logger.cs
@@ -0,0 +1,50 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froze.Core.Logging
+{
+    public class Logger : ILogger
+    {
+        private readonly ILog log = LogManager.GetLogger("LOGGER");
+        
+        public Logger()
+        {
+            InitLogger();
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        private void Write(string msg)
+        {
+            log.Info(msg);
+        }
+
+        public void Error(string msg)
+        {
+            Write(msg);
+        }
+
+        public void Error(Exception e)
+        {
+            Write(e.Message);
+        }
+
+        public void Info(string msg)
+        {
+            Write(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            Write(msg);
+        }
+    }
+}
